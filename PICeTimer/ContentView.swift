aa5345e8af8f5bf2aea0cd8c7c7614075fa4cf54
Dr Adrian Ward MRCVS <adrian.c.ward@gmail.com>
2019-07-21 22:10:54 +0100

    Initial Commit

---
+++ PICeTimer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  PICeTimer
+//
+//  Created by Adrian Ward on 21/07/2019.
+//  Copyright © 2019 Adrian Ward. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
